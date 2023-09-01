305cd032131561e01b7e1a01902b035cdcee113a
Amy Lam <amy.r.lam@gmail.com>
2023-08-31 18:21:21 -0600

    Set up 11ty
    README instructions: https://github.com/11ty/eleventy-base-blog

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Amy Lam",
+	url: "https://www.amyrlam.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Amy Lam's personal website",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "Amy Lam",
+		email: "hello@amyrlam.com",
+		url: "https://www.amyrlam.com/",
+	},
+};
