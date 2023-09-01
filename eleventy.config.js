d8107d291832d773525bc8d41b37989170bef9ba
Amy Lam <amy.r.lam@gmail.com>
2023-08-31 22:04:41 -0600

    Simplify build, since no pathPrefix
    Ref: https://discord.com/channels/741017160297611315/1147001367840440352
    
    Also, add .prettierignore due to error with global VSCode settings locally (debug later)

--- eleventy.config.js
+++ eleventy.config.js
@@ -124,17 +124,5 @@ module.exports = function(eleventyConfig) {
 			data: "../_data",          // default: "_data"
 			output: "_site"
 		},
-
-		// -----------------------------------------------------------------
-		// Optional items:
-		// -----------------------------------------------------------------
-
-		// If your site deploys to a subdirectory, change `pathPrefix`.
-		// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-		// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-		// it will transform any absolute URLs in your HTML to include this
-		// folder name and does **not** affect where things go in the output folder.
-		pathPrefix: "/",
 	};
 };
